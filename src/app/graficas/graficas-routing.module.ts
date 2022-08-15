1c5eafced060457d1a9745020c2759f389720cc9
Ayilk <srangel.h92@gmail.com>
2022-08-15 17:43:23 -0500

    Rutas y lazyload

--- src/app/graficas/graficas-routing.module.ts
+++ src/app/graficas/graficas-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { DonasHttpComponent } from './pages/donas-http/donas-http.component';
+import { DonasComponent } from './pages/donas/donas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {path: 'barra', component: BarrasComponent},
+      {path: 'barra-doble', component: BarrasDobleComponent},
+      {path: 'dona', component: DonasComponent},
+      {path: 'dona-http', component: DonasHttpComponent},
+      {path: '**', redirectTo: 'barra'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
