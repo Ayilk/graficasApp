559501df966d6b8004c9597d9428cc027a026a00
Ayilk <srangel.h92@gmail.com>
2022-08-15 18:22:18 -0500

    Grafica de barra

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgChartsModule } from 'ng2-charts';
 
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
 import { DonasComponent } from './pages/donas/donas.component';
 import { DonasHttpComponent } from './pages/donas-http/donas-http.component';
 import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
 
 
 @NgModule({
   declarations: [
     BarrasComponent,
+    BarrasDobleComponent,
     DonasComponent,  
     DonasHttpComponent,
     GraficaBarraComponent
   ],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    NgChartsModule
   ]
 })
 export class GraficasModule { }
